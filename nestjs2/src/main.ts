808b5839421dc1445073269dfcc701a5071b6b11
astmain <1311192345@qq.com>
2025-08-31 23:22:21 +0800

    111

--- nestjs2/src/main.ts
+++ nestjs2/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const { env_curr_back_description } = Plugins.check_env()
   await Plugins.filter_cors(app) // 配置跨域
   await Plugins.swagger_Knife4j(app) // 配置swagger
-  await app.listen(Number(process.env.PORT))
+  await app.listen(Number(process.env.VITE_port))
   console.log('启动成功:', env_curr_back_description)
 }
 
